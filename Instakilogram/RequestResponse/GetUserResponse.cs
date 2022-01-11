71f0617453fdf7e74cacbf57ab2272cc86f5cb77
dusanlazarevic-dev <73256535+dusanlazarevic-dev@users.noreply.github.com>
2022-01-11 19:59:39 +0100

    dodati userresponse i imageasbase64 fajlovi

---
+++ Instakilogram/RequestResponse/GetUserResponse.cs
@@ -0,0 +1,23 @@
+﻿using Instakilogram.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instakilogram.RequestResponse
+{
+    public class GetUserResponse
+    {
+        public GetUserResponse(IEnumerable<User> user, IEnumerable<Photo> uploadedPhotos, IEnumerable<Photo> taggedOnPhotos)
+        {
+            User = user;
+            UploadedPhotos = uploadedPhotos;
+            this.taggedOnPhotos = taggedOnPhotos;
+        }
+
+        public IEnumerable<User> User { get; set; }
+        public IEnumerable<Photo> UploadedPhotos { get; set; }
+        public IEnumerable<Photo> taggedOnPhotos { get; set; }
+
+    }
+}
---
+++ Instakilogram/RequestResponse/ImageAsBase64.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instakilogram
+{
+    public class ImageAsBase64
+    {
+        public string FileName { get; set; } 
+        public string Base64Content { get; set; }
+
+    }
+}
